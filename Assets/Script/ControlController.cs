a3738d9b1d3ec40f0d80c6905233ae14dac948e9
sammy310 <kipper098@gmail.com>
2020-11-21 01:15:26 +0900

    텔레포터 컨트롤 추가

---
+++ Assets/Script/ControlController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ControlController : MonoBehaviour
+{
+    static ControlController s_Instance = null;
+    public static ControlController Instance => s_Instance;
+
+    private SnapTurnProvider snapTurn;
+
+    void Awake()
+    {
+        s_Instance = this;
+        snapTurn = GetComponent<SnapTurnProvider>();
+    }
+
+    public void SetScripts(bool isEnable)
+    {
+        MasterController.Instance.LeftTeleportInteractor.gameObject.SetActive(isEnable);
+        MasterController.Instance.RightTeleportInteractor.gameObject.SetActive(isEnable);
+        MasterController.Instance.LeftTractorBeam.gameObject.SetActive(isEnable);
+        MasterController.Instance.RightTractorBeam.gameObject.SetActive(isEnable);
+
+        snapTurn.enabled = isEnable;
+    }
+}
