4af7aae8b905cae4d7c0027ed9de661d3e3a363c
okh555 <okh8522@gmail.com>
2020-11-21 02:52:19 +0900

    basket

--- Assets/Basket_Manager.cs
+++ Assets/Basket_Manager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Basket_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject scorePoint1;
+    public GameObject scorePoint2;
+
+    private ReturnBall point1;
+    private ReturnBall point2;
+
+    void Awake()
     {
-        
+        point1 = scorePoint1.GetComponent<ReturnBall>();
+        point2 = scorePoint2.GetComponent<ReturnBall>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Score();
     }
+
+    void Score()
+    {
+        if (point1.ball != null)
+        {
+            if (point1.returnBall() == point2.returnBall())
+            {
+                Debug.Log("Score");
+                point1.ball = null;
+                point2.ball = null;
+            }
+        }
+    }
+
+
 }
