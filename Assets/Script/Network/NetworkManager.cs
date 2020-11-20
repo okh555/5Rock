2b0d2afece6ce6681ab201c2603d507d8ba60d45
ewsnybchan7 <50603204+ewsnybchan7@users.noreply.github.com>
2020-11-21 02:47:11 +0900

    add photon
    photon 추가
    lobby scene 추가
    network player 추가

---
+++ Assets/Script/Network/NetworkManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+[System.Serializable]
+public class VRRoom
+{
+    public string name;
+    public int maxPlayer;
+}
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public List<VRRoom> roomList;
+    public GameObject roomUI;
+
+    public void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect To Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server.");
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("WE JOINED THE LOBBY");
+        roomUI.SetActive(true);
+    }
+
+    public void InitializeRoom(int roomIndex)
+    {
+        VRRoom roomSettings = roomList[roomIndex];
+
+        // Load Scene
+        PhotonNetwork.LoadLevel(1);
+
+        // Create the Room
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)roomSettings.maxPlayer;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom(roomSettings.name, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the Room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
---
+++ Assets/Script/Network/NetworkPlayer.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
+
+    private XRRig xrRig;
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
+
+    private XRController leftController;
+    private XRController rightController;
+
+    private PhotonView photonView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        xrRig = FindObjectOfType<XRRig>();
+
+        headRig = xrRig.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = xrRig.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandRig = xrRig.transform.Find("Camera Offset/RightHand Controller");
+
+        leftController = leftHandRig.GetComponent<XRController>();
+        rightController = rightHandRig.GetComponent<XRController>();
+
+        if (photonView.IsMine)
+        {
+            foreach(var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            MapPosition(this.transform, xrRig.transform);
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+
+            // animation
+            UpdateHandAnimation(leftController, leftHandAnimator);
+            UpdateHandAnimation(rightController, rightHandAnimator);
+        }
+    }
+
+    private void MapPosition(Transform target, Transform rigTransform)
+    {
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
+    }
+
+    private void UpdateHandAnimation(XRController targetDevice, Animator handAnimator)
+    {
+        if (targetDevice.selectInteractionState.activatedThisFrame)
+            handAnimator.SetTrigger(targetDevice.modelSelectTransition);
+        else if (targetDevice.selectInteractionState.deActivatedThisFrame)
+            handAnimator.SetTrigger(targetDevice.modelDeSelectTransition);  
+    }
+}
