3a5c7200f7ae0078298499b2b40fc46dd9656c25
ewsnybchan7 <50603204+ewsnybchan7@users.noreply.github.com>
2020-11-21 03:07:43 +0900

    network hand animation
    remark animation

--- Assets/Script/Network/NetworkPlayer.cs
+++ Assets/Script/Network/NetworkPlayer.cs
@@ -70,9 +70,9 @@ public class NetworkPlayer : MonoBehaviour
 
     private void UpdateHandAnimation(XRController targetDevice, Animator handAnimator)
     {
-        if (targetDevice.selectInteractionState.activatedThisFrame)
-            handAnimator.SetTrigger(targetDevice.modelSelectTransition);
-        else if (targetDevice.selectInteractionState.deActivatedThisFrame)
-            handAnimator.SetTrigger(targetDevice.modelDeSelectTransition);  
+        //if (targetDevice.selectInteractionState.activatedThisFrame)
+        //    handAnimator.SetTrigger(targetDevice.modelSelectTransition);
+        //else if (targetDevice.selectInteractionState.deActivatedThisFrame)
+        //    handAnimator.SetTrigger(targetDevice.modelDeSelectTransition);  
     }
 }
