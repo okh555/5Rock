1c03f207d271e33d1637e9fc01d3da645edbe3d6
okh555 <okh8522@gmail.com>
2020-11-23 21:57:44 +0900

    changed

--- Assets/Script/Network/NetworkPlayer.cs
+++ Assets/Script/Network/NetworkPlayer.cs
@@ -70,16 +70,13 @@ public class NetworkPlayer : MonoBehaviour
 
     private void UpdateHandAnimation(XRController targetDevice, Animator handAnimator)
     {
-<<<<<<< Updated upstream
         //if (targetDevice.selectInteractionState.activatedThisFrame)
         //    handAnimator.SetTrigger(targetDevice.modelSelectTransition);
         //else if (targetDevice.selectInteractionState.deActivatedThisFrame)
         //    handAnimator.SetTrigger(targetDevice.modelDeSelectTransition);  
-=======
        /* if (targetDevice.selectInteractionState.activatedThisFrame)
             handAnimator.SetTrigger(targetDevice.modelSelectTransition);
         else if (targetDevice.selectInteractionState.deActivatedThisFrame)
             handAnimator.SetTrigger(targetDevice.modelDeSelectTransition);  */
->>>>>>> Stashed changes
     }
 }
