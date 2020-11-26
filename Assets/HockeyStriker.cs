c7685a388a915d399a332cab91bed2bcabdf9fce
okh555 <okh8522@gmail.com>
2020-11-26 15:12:44 +0900

    Init_gayoung

---
+++ Assets/HockeyStriker.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HockeyStriker : XRGrabableObject
+{
+
+    public Transform currentPos;
+   
+
+    protected override void OnSelectEnter(XRBaseInteractor interactor)
+    {
+        base.OnSelectEnter(interactor);
+        
+
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+        switch (updatePhase)
+        {
+            case XRInteractionUpdateOrder.UpdatePhase.Dynamic:
+                {
+                    if (isSelected)
+                    {
+                        transform.position = new Vector3(transform.position.x, currentPos.position.y, transform.position.z);
+                    }
+                }
+                break;
+            case XRInteractionUpdateOrder.UpdatePhase.OnBeforeRender:
+                {
+                    if (isSelected)
+                    {
+                        transform.position = new Vector3(transform.position.x, currentPos.position.y, transform.position.z);
+                    }
+                }
+                break;
+            case XRInteractionUpdateOrder.UpdatePhase.Late:
+                {
+                    if (isSelected)
+                    {
+                        Debug.Log("3");
+                    }
+                }
+                break;
+        }
+      
+    }
+    }
---
+++ Assets/Puck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puck : MonoBehaviour
+{
+    Vector3 currentPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPos = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y != currentPos.y)
+        {
+            transform.position = new Vector3(transform.position.x, currentPos.y, transform.position.z);
+        }
+    }
+}
