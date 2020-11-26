1b9efddc748c0a358a1b21a6ed7e2d544eddf459
okh555 <okh8522@gmail.com>
2020-11-26 22:53:58 +0900

    에어하키 추가

--- Assets/HockeyStriker.cs
+++ Assets/HockeyStriker.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class HockeyStriker : XRGrabableObject
@@ -18,33 +17,10 @@ public class HockeyStriker : XRGrabableObject
     public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
     {
         base.ProcessInteractable(updatePhase);
-        switch (updatePhase)
-        {
-            case XRInteractionUpdateOrder.UpdatePhase.Dynamic:
-                {
-                    if (isSelected)
-                    {
-                        transform.position = new Vector3(transform.position.x, currentPos.position.y, transform.position.z);
-                    }
-                }
-                break;
-            case XRInteractionUpdateOrder.UpdatePhase.OnBeforeRender:
-                {
-                    if (isSelected)
-                    {
-                        transform.position = new Vector3(transform.position.x, currentPos.position.y, transform.position.z);
-                    }
-                }
-                break;
-            case XRInteractionUpdateOrder.UpdatePhase.Late:
-                {
-                    if (isSelected)
-                    {
-                        Debug.Log("3");
-                    }
-                }
-                break;
-        }
+
+        transform.position = new Vector3(transform.position.x, currentPos.position.y, transform.position.z);
+            
       
     }
-    }
+
+}
