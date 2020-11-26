0453b334f1213b15d8f572384e76078038242542
leegayoung2 <70629251+leegayoung2@users.noreply.github.com>
2020-11-26 21:55:34 +0900

    당구공 스폰 스크립트 추가

---
+++ Assets/Billiards/Scripts/SpawnBilliardBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBilliardBall : MonoBehaviour
+{
+    public GameObject balls;
+    public Transform spawnPosition;
+
+    private GameObject spawnedBalls;
+
+    public void SpawnBall()
+    {
+        if (spawnedBalls != null)
+        {
+            Destroy(spawnedBalls);
+        }
+
+        spawnedBalls = Instantiate(balls);
+        spawnedBalls.transform.position = spawnPosition.position;
+    }
+}
