676938d936f8957d9cbfb65bffe6a7c51fb5e382
okh555 <okh8522@gmail.com>
2020-12-03 15:40:57 +0900

    airHockey

--- Assets/HockeyStriker.cs
+++ Assets/HockeyStriker.cs
@@ -7,6 +7,10 @@ public class HockeyStriker : XRGrabableObject
     public Vector3 currentPos;
     public Quaternion currentRot;
 
+    Vector3 PrevPos;
+    Vector3 NewPos;
+    Vector3 ObjVelocity;
+
     Rigidbody rig;
    
 
@@ -15,6 +19,9 @@ public class HockeyStriker : XRGrabableObject
         currentPos = GetComponent<Transform>().position;
         currentRot = GetComponent<Transform>().rotation;
         rig = GetComponent<Rigidbody>();
+
+        PrevPos = transform.position;
+        NewPos = transform.position;
     }
 
     protected override void OnSelectEnter(XRBaseInteractor interactor)
@@ -29,20 +36,45 @@ public class HockeyStriker : XRGrabableObject
         base.ProcessInteractable(updatePhase);
         transform.position = new Vector3(transform.position.x, currentPos.y, transform.position.z);
         transform.rotation = new Quaternion(currentRot.x, currentRot.y, currentRot.z, currentRot.w);
-        
+
+        NewPos = transform.position;  // each frame track the new position
+        ObjVelocity = (NewPos - PrevPos) / Time.fixedDeltaTime;  // velocity = dist/time
+        PrevPos = NewPos;  // update position for next frame calculation
+
+
     }
 
     void OnTriggerEnter(Collider collider)
     {
+        if (rig.velocity.magnitude < .01)
+        {
+            rig.velocity = Vector3.zero;
+            rig.angularVelocity = Vector3.zero;
+        }
+
         if (collider.gameObject.tag == "Wall")
         {
             if (collider.gameObject.name == "Wall1")
             {
+                Debug.Log(rig.velocity);
                 rig.velocity = new Vector3(rig.velocity.x, 0, -rig.velocity.z);
+                rig.velocity *= 0.8f;
             }
-            else
+            if (collider.gameObject.name == "Wall2")
+            {
+                Debug.Log(rig.velocity);
+                rig.velocity = new Vector3(-rig.velocity.x, 0, rig.velocity.z);
+                rig.velocity *= 0.8f;
+            }
+            if (collider.gameObject.name == "Score")
             {
                 rig.velocity = new Vector3(-rig.velocity.x, 0, rig.velocity.z);
+                rig.velocity *= 0.8f;
+            }
+            if (collider.gameObject.name == "Score2")
+            {
+                rig.velocity = new Vector3(-rig.velocity.x, 0, rig.velocity.z);
+                rig.velocity *= 0.8f;
             }
         }
     }
